0819c52e106967050d55e2e0b178f77ac2612bc6
Roman Dronov <dr.meamuri@gmail.com>
2023-04-11 22:08:21 +0200

    Add clj repl package

--- lua/clozric/packer.lua
+++ lua/clozric/packer.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('Olical/conjure')
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
