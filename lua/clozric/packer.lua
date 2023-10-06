eca7d5526f2643ee904dd13e9e447e104ba99ae9
Roman Dronov <dr.meamuri@gmail.com>
2023-10-06 21:04:12 +0200

    Bump lsp-zero version

--- lua/clozric/packer.lua
+++ lua/clozric/packer.lua
@@ -43,24 +43,20 @@ return require('packer').startup(function(use)
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
 	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
 		  {'williamboman/mason.nvim'},
 		  {'williamboman/mason-lspconfig.nvim'},
 
-		  -- Autocompletion
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+
+          -- Autocompletion
 		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
 		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
 
 		  -- Snippets
 		  {'L3MON4D3/LuaSnip'},
-		  -- Snippet Collection (Optional)
-		  {'rafamadriz/friendly-snippets'},
 	  }
   }
   use('ziglang/zig.vim')
